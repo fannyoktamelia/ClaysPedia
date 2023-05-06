e5019983dbbbf619a40346af987f307a3b28d641
fannyoktamelia <91531375+fannyoktamelia@users.noreply.github.com>
2023-05-06 22:00:02 +0700

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'softon/indipay' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Softon\\Indipay\\IndipayServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Indipay' => 'Softon\\Indipay\\Facades\\Indipay',
+    ),
+  ),
+);
